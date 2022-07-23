6ba558c9e270862dbf7339f96889177e0eca300b
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-07-23 13:05:00 +0200

    edit uri's

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,8 +19,8 @@ const credentials = {
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
   token_uri: 'https://oauth2.googleapis.com/token',
   auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://tktamir.github.io/meetout/'],
-  javascript_origins: ['https://tktamir.github.io', 'http://localhost:3000'],
+  redirect_uris: ['https://TKTamir.github.io/meetout/'],
+  javascript_origins: ['https://TKTamir.github.io', 'http://localhost:3000'],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
